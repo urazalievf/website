01d7c16ba9dc1a2e2b57a27f52279fa925573456
Feruz Urazaliev <feruz_u@icloud.com>
2024-07-02 12:50:03 -0500

    Major Upgrade to the new site

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const hamburger = document.querySelector('.hamburger');
-    const navLinks = document.querySelector('.nav-links');
+    fetch('header.html')
+        .then(response => response.text())
+        .then(data => {
+            document.querySelector('header').innerHTML = data;
+            setupNavigation();
+        });
 
-    hamburger.addEventListener('click', () => {
-        navLinks.classList.toggle('active');
-    });
+    fetch('footer.html')
+        .then(response => response.text())
+        .then(data => {
+            document.querySelector('footer').innerHTML = data;
+        });
 
-    // Contact form submission
-    const form = document.getElementById('contact-form');
-    if (form) {
-        form.addEventListener('submit', function (event) {
-            event.preventDefault();
+    function setupNavigation() {
+        const hamburger = document.querySelector('.hamburger');
+        const navLinks = document.querySelector('.nav-links');
+        const dropdown = document.querySelector('.dropdown');
 
-            const formData = new FormData(form);
-            const data = {
-                'entry.1668657256': formData.get('name'),
-                'entry.73016882': formData.get('email'),
-                'entry.1166974658': formData.get('message')
-            };
+        hamburger.addEventListener('click', () => {
+            navLinks.classList.toggle('active');
+        });
 
-            fetch('https://docs.google.com/forms/d/e/196Ptnu_XIGdiehgJZdGnHh7srOXQJPR5fpGsPJYanBQ/formResponse', {
-                method: 'POST',
-                mode: 'no-cors',
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded'
-                },
-                body: new URLSearchParams(data).toString()
-            })
-            .then(() => {
-                document.getElementById('form-response').innerText = 'Thank you for your message!';
-                form.reset();
-            })
-            .catch(() => {
-                document.getElementById('form-response').innerText = 'There was a problem submitting your message. Please try again later.';
-            });
+        navLinks.addEventListener('click', (event) => {
+            if (event.target.closest('li').querySelector('.dropdown')) {
+                dropdown.classList.toggle('show');
+            }
         });
     }
 });
